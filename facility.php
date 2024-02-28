9c591febeaab17775608fe25ee9d1834dff9408e
Eshan Adithaya <87906517+EshanAdithaya@users.noreply.github.com>
2024-02-28 21:22:33 +0530

    deploy

---
+++ facility.php
@@ -0,0 +1,263 @@
+<?php 
+include 'header.php';
+?>
+        <!-- main part -->
+        <main id="content" class="site-main">
+            <!-- Inner Banner html start-->
+            <section class="inner-banner-wrap">
+                <div class="inner-baner-container" style="background-image: url(assets/img/educator-img12.jpg);">
+                    <div class="container">
+                        <div class="inner-banner-content">
+                            <h1 class="inner-title">Facility</h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Inner Banner html end-->
+            <!-- facility page html start -->
+            <!-- home feature section html start -->
+            <section class="home-feature-section">
+                <div class="container">
+                    <div class="feature-wrapper">
+                        <div class="feature-type">
+                            <div class="feature-icon">
+                                <h5 class="meta-no">1</h5>
+                                <i aria-hidden="true" class="icon icon-management"></i>
+                            </div>
+                            <h4 class="feature-heading">Skilled Teachers</h4>
+                            <p class="feature-info">Dictumst magni irure nascetur nam primis, aliquam ipsum. Element, occaecat rerum .</p>
+                            <a href="#" class="right-arrow-link">
+                                <i aria-hidden="true" class="icon icon-right-arrow"></i>
+                            </a>
+                        </div>
+                        <div class="feature-type">
+                            <div class="feature-icon">
+                                <h5 class="meta-no">2</h5>
+                                <i aria-hidden="true" class="icon icon-Money-2"></i>
+                            </div>
+                            <h4 class="feature-heading">Affordable Courses</h4>
+                            <p class="feature-info">Dictumst magni irure nascetur nam primis, aliquam ipsum. Element, occaecat rerum .</p>
+                            <a href="#" class="right-arrow-link">
+                                <i aria-hidden="true" class="icon icon-right-arrow"></i>
+                            </a>
+                        </div>
+                        <div class="feature-type">
+                            <div class="feature-icon">
+                                <h5 class="meta-no">3</h5>
+                                <i aria-hidden="true" class="icon icon-atomic"></i>
+                            </div>
+                            <h4 class="feature-heading">Efficient & Flexible</h4>
+                            <p class="feature-info">Dictumst magni irure nascetur nam primis, aliquam ipsum. Element, occaecat rerum .</p>
+                            <a href="#" class="right-arrow-link">
+                                <i aria-hidden="true" class="icon icon-right-arrow"></i>
+                            </a>
+                        </div>
+                        <div class="feature-type">
+                            <div class="feature-icon">
+                                <h5 class="meta-no">4</h5>
+                                <i aria-hidden="true" class="icon icon-Safe-house"></i>
+                            </div>
+                            <h4 class="feature-heading">Friendly Place</h4>
+                            <p class="feature-info">Dictumst magni irure nascetur nam primis, aliquam ipsum. Element, occaecat rerum .</p>
+                            <a href="#" class="right-arrow-link">
+                                <i aria-hidden="true" class="icon icon-right-arrow"></i>
+                            </a>
+                        </div>
+                        <div class="feature-type">
+                            <div class="feature-icon">
+                                <h5 class="meta-no">5</h5>
+                                <i aria-hidden="true" class="icon icon-diploma"></i>
+                            </div>
+                            <h4 class="feature-heading">Global Certification</h4>
+                            <p class="feature-info">Dictumst magni irure nascetur nam primis, aliquam ipsum. Element, occaecat rerum .</p>
+                            <a href="#" class="right-arrow-link">
+                                <i aria-hidden="true" class="icon icon-right-arrow"></i>
+                            </a>
+                        </div>
+                        <div class="feature-type">
+                            <div class="feature-icon">
+                                <h5 class="meta-no">6</h5>
+                                <i aria-hidden="true" class="icon icon-Support-2"></i>
+                            </div>
+                            <h4 class="feature-heading">Best Support</h4>
+                            <p class="feature-info">Dictumst magni irure nascetur nam primis, aliquam ipsum. Element, occaecat rerum .</p>
+                            <a href="#" class="right-arrow-link">
+                                <i aria-hidden="true" class="icon icon-right-arrow"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- facility page html end -->
+        </main>
+        <!-- footer part -->
+        <footer id="colophon" class="site-footer">
+            <div class="footer-overlay"></div>
+            <div class="subscribe-section">
+                <div class="container">
+                    <div class="subscribe-content">
+                        <div class="overlay"></div>
+                        <div class="row align-items-center">
+                            <div class="col-lg-5">
+                                <h3 class="subscribe-title">Subscribe Our Newsletter !</h3>
+                                <p class="subscribe-info">Elementum ex similique sollicitudin eveniet sem eveniet proin, iste euismod! Quia! Fugiat molestie leo placerat.</p>
+                            </div>
+                            <div class="col-lg-7">
+                                <div class="form-content">
+                                    <input type="email" name="email" placeholder="Enter Email Address">
+                                    <button type="submit" class="button-round-primary">SUBSCRIBE</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="top-footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6">
+                            <aside class="widget widget_text img-textwidget">
+                                <div class="footer-logo">
+                                    <a href="index.html"><img src="assets/img/educator-logo1.png" alt="logo"></a>
+                                </div>
+                                <div class="textwidget widget-text">
+                                    Vero, tempor morbi, adipiscing aliqua nonummy proident perferendis? Laboris lacus quidem repellendus quasi.
+                                </div>
+                            </aside>
+                            <div class="footer-social-links">
+                                <ul>
+                                    <li>
+                                        <a href="https://www.facebook.com/" target="_blank">
+                                            <i class="fab fa-facebook-f" aria-hidden="true"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.twitter.com/" target="_blank">
+                                            <i class="fab fa-twitter" aria-hidden="true"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.instagram.com/" target="_blank">
+                                            <i class="fab fa-instagram" aria-hidden="true"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="https://www.youtube.com/" target="_blank">
+                                            <i class="fab fa-youtube" aria-hidden="true"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <aside class="widget">
+                                <h5 class="widget-title">Quick LInks</h5>
+                                <ul>
+                                    <li>
+                                        <a href="about.html">About us</a>
+                                    </li>
+                                    <li>
+                                        <a href="career.html">Careers</a>
+                                    </li>
+                                    <li>
+                                        <a href="single-blog.html">News & Articles</a>
+                                    </li>
+                                    <li>
+                                        <a href="legal-notice.html">Legal Notice</a>
+                                    </li>
+                                </ul>
+                            </aside>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <aside class="widget">
+                                <h5 class="widget-title">Support</h5>
+                                <ul>
+                                    <li>
+                                        <a href="faq.html">Help Center</a>
+                                    </li>
+                                    <li>
+                                        <a href="contact.html">Contact Us</a>
+                                    </li>
+                                    <li>
+                                        <a href="product-cart.html">Payment Center</a>
+                                    </li>
+                                    <li>
+                                        <a href="about.html">Parent Community</a>
+                                    </li>
+                                </ul>
+                            </aside>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <aside class="widget widget_text school-time">
+                                <h5 class="widget-title">School Hours</h5>
+                                <span>
+                                    <i aria-hidden="true" class="far fa-clock"></i>
+                                    8 AM - 5 PM , Monday - Saturday
+                                </span>
+                                <p>Aut, quae convallis minim cum ornare! Pede dictum convallis.</p>
+                                <a href="contact.html" class="button-round-secondary ">JOIN US NOW</a>
+                            </aside>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="bottom-footer">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="copy-right">Copyright &copy; 2023 Educator. All rights reserved.</div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="legal-list">
+                                <ul>
+                                    <li>
+                                        <a href="legal-notice.html">PRIVACY POLICY</a>
+                                    </li>
+                                    <li>
+                                        <a href="contact.html">SUPPORT</a>
+                                    </li>
+                                    <li>
+                                        <a href="legal-notice.html">TERMS & CONDITION</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- back to top -->
+        <a id="backTotop" href="#" class="to-top-icon">
+            <i class="fas fa-chevron-up"></i>
+        </a>
+        <!-- custom search field html -->
+        <div class="header-search-form">
+            <div class="container">
+                <div class="header-search-container">
+                    <form class="search-form" role="search" method="get">
+                        <input type="text" name="s" placeholder="Enter your text...">
+                    </form>
+                    <a href="#" class="search-close">
+                        <i class="fas fa-times"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- JavaScript -->
+    <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="assets/vendors/jquery/jquery.js"></script>
+    <script src="assets/vendors/waypoint/jquery.waypoints.min.js"></script>
+    <script src="assets/vendors/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/vendors/jquery-ui/jquery-ui.min.js"></script>
+    <script src="assets/vendors/progressbar-fill-visible/js/progressBar.min.js"></script>
+    <script src="assets/vendors/countdown-date-loop-counter/loopcounter.js"></script>
+    <script src="assets/vendors/counterup/jquery.counterup.js"></script>
+    <script src="assets/vendors/modal-video/jquery-modal-video.min.js"></script>
+    <script src="assets/vendors/masonry/masonry.pkgd.min.js"></script>
+    <script src="assets/vendors/fancybox/dist/jquery.fancybox.min.js"></script>
+    <script src="assets/vendors/slick/slick.min.js"></script>
+    <script src="assets/vendors/slick-nav/jquery.slicknav.js"></script>
+    <script src="assets/js/custom.js"></script>
+</body>
+
+</html>
