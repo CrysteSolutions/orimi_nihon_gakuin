7017919fc601fddb8304e65d3daa2d1dcccb5597
Eshan Adithaya <87906517+EshanAdithaya@users.noreply.github.com>
2024-03-05 13:07:21 +0530

    delete all the buggy code

--- assets/js/guard.js
+++
@@ -1,9 +0,0 @@
-<script>
-// On page load or when changing themes, best to add inline in `head` to avoid FOUC
-if (localStorage.getItem("theme-color") === "dark" || (!("theme-color" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
-  document.getElementById("light--to-dark-button")?.classList.add("dark--mode");
-} 
-if (localStorage.getItem("theme-color") === "light") {
-  document.getElementById("light--to-dark-button")?.classList.remove("dark--mode");
-} 
-</script>
